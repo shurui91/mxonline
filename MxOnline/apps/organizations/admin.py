8140c6304fed91340535de8a174ca28ee474792c
Aaron Liu <shurui91@gmail.com>
2023-04-04 21:20:50 -0700

    organization model added to admin panel

--- MxOnline/apps/organizations/admin.py
+++ MxOnline/apps/organizations/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from MxOnline.apps.organizations.models import Teacher, CourseOrg, City
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    pass
+
+
+class CourseOrgAdmin(admin.ModelAdmin):
+    pass
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "desc"]
+    search_fields = ["name", "desc"]
+
+
+# admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(CourseOrg, CourseOrgAdmin)
+admin.site.register(City, CityAdmin)
--- MxOnline/apps/organizations/apps.py
+++ MxOnline/apps/organizations/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class OrganizationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'MxOnline.apps.organizations'
+    verbose_name = '机构管理'
