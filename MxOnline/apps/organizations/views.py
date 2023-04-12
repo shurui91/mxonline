0349759ed7a269e9bd7410130fccd12d6daf489f
Aaron Liu <shurui91@gmail.com>
2023-04-12 12:59:12 -0700

    show cities on org_list page

--- MxOnline/apps/organizations/views.py
+++ MxOnline/apps/organizations/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 
-from MxOnline.apps.organizations.models import CourseOrg
+from MxOnline.apps.organizations.models import CourseOrg, City
 from MxOnline.settings import MEDIA_URL
 
 
 class OrgView(View):
     def get(self, request, *args, **kwargs):
         all_orgs = CourseOrg.objects.all()
+        org_nums = CourseOrg.objects.count()
+        all_cities = City.objects.all()
         return render(request, 'org-list.html', {
             "all_orgs": all_orgs,
-            "MEDIA_URL": MEDIA_URL,
+            "org_nums": org_nums,
+            "all_cities": all_cities,
         })
