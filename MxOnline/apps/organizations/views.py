0983d0c77153ea5cb2e6fedd282410dd115d972e
Aaron Liu <shurui91@gmail.com>
2023-04-13 16:36:03 -0700

    org page can filter by org category

--- MxOnline/apps/organizations/views.py
+++ MxOnline/apps/organizations/views.py
@@ -7,9 +7,17 @@ from MxOnline.settings import MEDIA_URL
 
 class OrgView(View):
     def get(self, request, *args, **kwargs):
+        # 从数据库获取数据
         all_orgs = CourseOrg.objects.all()
-        org_nums = CourseOrg.objects.count()
         all_cities = City.objects.all()
+
+        # 对课程机构进行筛选
+        category = request.GET.get("ct", "")
+        if category:
+            all_orgs = all_orgs.filter(category=category)
+
+        org_nums = all_orgs.count()
+
         return render(request, 'org-list.html', {
             "all_orgs": all_orgs,
             "org_nums": org_nums,
