043d559a5b87da981173042fe5cc650192189145
Aaron Liu <shurui91@gmail.com>
2023-04-14 14:56:51 -0700

    sidebar form POST bug fixed

--- MxOnline/apps/organizations/urls.py
+++ MxOnline/apps/organizations/urls.py
@@ -1,8 +1,8 @@
 from django.urls import re_path as url
-from MxOnline.apps.organizations.views import OrgView
+from MxOnline.apps.organizations.views import OrgView, AddAskView
 
 urlpatterns = [
     url(r'^list/$', OrgView.as_view(), name="list"),
-    url(r'^add_ask/$', OrgView.as_view(), name="add_ask"),
+    url(r'^add_ask/$', AddAskView.as_view(), name="add_ask"),
 
 ]
