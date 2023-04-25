b284779108706aaf2acc5373f905b6a2344b4e68
Aaron Liu <shurui91@gmail.com>
2023-04-24 19:47:27 -0700

    related course recommendation sidebar is done

--- MxOnline/apps/courses/views.py
+++ MxOnline/apps/courses/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import View
-from MxOnline.apps.courses.models import Course
+from MxOnline.apps.courses.models import Course, CourseTag
 from MxOnline.apps.operations.models import UserFavorite
 
 
@@ -15,22 +15,31 @@ class CourseDetailView(View):
         has_fav_course = False
         has_fav_org = False
 
-        # 通过课程的tag做课程推荐
-        tag = course.tag
-        related_courses = []
-        if tag:
-            related_courses = Course.objects.filter(tag=tag).exclude(id__in=[course.id])[:3]
-
         if request.user.is_authenticated:
             if UserFavorite.objects.filter(user=request.user, fav_id=course.id, fav_type=1):
                 has_fav_course = True
             if UserFavorite.objects.filter(user=request.user, fav_id=course.course_org.id, fav_type=2):
                 has_fav_org = True
 
+        # 通过课程的tag做课程推荐
+        # tag = course.tag
+        # related_courses = []
+        # if tag:
+        #     related_courses = Course.objects.filter(tag=tag).exclude(id__in=[course.id])[:3]
+        tags = course.coursetag_set.all()
+        tag_list = [tag.tag for tag in tags]
+
+        course_tags = CourseTag.objects.filter(tag__in=tag_list).exclude(course__id=course.id)
+        # 为了避免两个课程有多个相同的tag
+        related_courses = set()
+        for course_tag in course_tags:
+            related_courses.add(course_tag.course)
+
         return render(request, "course-detail.html", {
             "course": course,
             "has_fav_course": has_fav_course,
             "has_fav_org": has_fav_org,
+            "course_tags": course_tags,
             "related_courses": related_courses,
         })
 
