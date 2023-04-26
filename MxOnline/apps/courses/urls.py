8460a68647672f2f7ca7a640f123a9a11f38f948
Aaron Liu <shurui91@gmail.com>
2023-04-26 10:11:11 -0700

    course comment tab css part is done

--- MxOnline/apps/courses/urls.py
+++ MxOnline/apps/courses/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.urls import re_path as url
 
-from MxOnline.apps.courses.views import CourseListView, CourseDetailView, CourseLessonView
+from MxOnline.apps.courses.views import CourseListView, CourseDetailView, CourseLessonView, CourseCommentsView
 
 urlpatterns = [
     url(r'^list/$', CourseListView.as_view(), name="list"),
     url(r'^(?P<course_id>\d+)/$', CourseDetailView.as_view(), name="detail"),
     url(r'^(?P<course_id>\d+)/lesson/$', CourseLessonView.as_view(), name="lesson"),
+    url(r'^(?P<course_id>\d+)/comments/$', CourseCommentsView.as_view(), name="comments"),
 ]
